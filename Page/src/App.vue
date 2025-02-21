571429a05f170fd50787f39f852f8f4f758ddbcb
ElectroHeavenVN <98677996+ElectroHeavenVN@users.noreply.github.com>
2025-02-21 08:20:17 +0700

    dislay last workflow status

--- Page/src/App.vue
+++ Page/src/App.vue
@@ -24,19 +24,22 @@ const { t } = useI18n();
     <nav>
       <div class="wrapper">
         <div href="/" class="content head">
-          <span v-if="currentPath == 'TeaMobi' || currentPath == 'HSNR'" style="font-size: 25px; cursor:pointer; position: relative; top:1px;" @click=openNav>&#9776;</span>
+          <span v-if="currentPath == 'TeaMobi' || currentPath == 'HSNR'"
+            style="font-size: 25px; cursor:pointer; position: relative; top:1px;" @click=openNav>&#9776;</span>
           <a v-if="currentPath == 'HSNR'" href="/DataNRO/HSNR/">
             <img src="/DataHSNR.png" alt="HSNR">
           </a>
-          <a v-else-if="currentPath == 'TeaMobi'"  href="/DataNRO/TeaMobi/">
-          <img src="/DataNRO.png" alt="TeaMobi">
+          <a v-else-if="currentPath == 'TeaMobi'" href="/DataNRO/TeaMobi/">
+            <img src="/DataNRO.png" alt="TeaMobi">
           </a>
           <div style="display: flex; flex-direction: column; align-items: center;">
             <a href="/DataNRO/">
               <h1>{{ title }}</h1>
             </a>
             <a href="https://hits.seeyoufarm.com" style="position: relative; top: -5px;" target="_blank">
-              <img :src="'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Felectroheavenvn.github.io%2FDataNRO%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&edge_flat=false&title=' + encodeURIComponent(t('visits'))" style="width: auto; height: 12px;"/>
+              <img
+                :src="'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Felectroheavenvn.github.io%2FDataNRO%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&edge_flat=false&title=' + encodeURIComponent(t('visits'))"
+                style="width: auto; height: 12px;" />
             </a>
           </div>
           <a v-if="currentPath == ''" href="/DataNRO/TeaMobi/">
@@ -46,16 +49,53 @@ const { t } = useI18n();
             <img src="/DataHSNR.png" alt="HSNR">
           </a>
         </div>
-          <a v-if="currentWorkflowLink !== ''" class="workflowRunning content links" :href="currentWorkflowLink" target="_blank" :title="t('workflowCurrentlyUpdatingDesc')">
-          <span>
-            {{ t("workflowCurrentlyUpdating") }}
-          </span>
-          <svg width="20px" height="20px" fill="none" viewBox="0 0 16 16" class="anim-rotate" xmlns="http://www.w3.org/2000/svg">
-            <path fill="none" stroke="#DBAB0A" stroke-width="2" d="M3.05 3.05a7 7 0 1 1 9.9 9.9 7 7 0 0 1-9.9-9.9Z" opacity=".5"></path>
-            <path fill="#DBAB0A" fill-rule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Z" clip-rule="evenodd"></path>
-            <path fill="#DBAB0A" d="M14 8a6 6 0 0 0-6-6V0a8 8 0 0 1 8 8h-2Z"></path>
-          </svg>
-        </a>
+        <div class="lastWorkflow">
+          <a v-if="lastWorkflowStatus === 'in_progress'" class="content links" :href="lastWorkflowLink" target="_blank"
+            :title="t('workflowRunningDesc')">
+            <span>
+              {{ t("workflowRunning") }}
+            </span>
+            <svg width="20px" height="20px" fill="none" viewBox="0 0 16 16" class="anim-rotate"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill="none" stroke="#DBAB0A" stroke-width="2" d="M3.05 3.05a7 7 0 1 1 9.9 9.9 7 7 0 0 1-9.9-9.9Z"
+                opacity=".5"></path>
+              <path fill="#DBAB0A" fill-rule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Z" clip-rule="evenodd"></path>
+              <path fill="#DBAB0A" d="M14 8a6 6 0 0 0-6-6V0a8 8 0 0 1 8 8h-2Z"></path>
+            </svg>
+          </a>
+          <a v-else-if="lastWorkflowStatus === 'success'" class="content links" :href="lastWorkflowLink" target="_blank"
+            :title="t('workflowSuccessDesc')">
+            <span>
+              {{ t("workflowSuccess") }}
+            </span>
+            <svg width="16" height="16" style="margin-top: 2px"
+              class="octicon octicon-check-circle-fill color-fg-success" viewBox="0 0 16 16" version="1.1" role="img">
+              <path fill="#2BD853"
+                d="M8 16A8 8 0 1 1 8 0a8 8 0 0 1 0 16Zm3.78-9.72a.751.751 0 0 0-.018-1.042.751.751 0 0 0-1.042-.018L6.75 9.19 5.28 7.72a.751.751 0 0 0-1.042.018.751.751 0 0 0-.018 1.042l2 2a.75.75 0 0 0 1.06 0Z">
+              </path>
+            </svg>
+          </a>
+          <a v-else-if="lastWorkflowStatus === 'failure'" class="content links" :href="lastWorkflowLink" target="_blank"
+            :title="t('workflowFailureDesc')">
+            <span>
+              {{ t("workflowFailure") }}
+            </span>
+            <svg width="16" height="16" style="margin-top: 2px" class="octicon octicon-x-circle-fill color-fg-danger" viewBox="0 0 16 16" version="1.1" role="img">
+              <path fill="#FF9492"
+                d="M2.343 13.657A8 8 0 1 1 13.658 2.343 8 8 0 0 1 2.343 13.657ZM6.03 4.97a.751.751 0 0 0-1.042.018.751.751 0 0 0-.018 1.042L6.94 8 4.97 9.97a.749.749 0 0 0 .326 1.275.749.749 0 0 0 .734-.215L8 9.06l1.97 1.97a.749.749 0 0 0 1.275-.326.749.749 0 0 0-.215-.734L9.06 8l1.97-1.97a.749.749 0 0 0-.326-1.275.749.749 0 0 0-.734.215L8 6.94Z">
+              </path>
+            </svg>
+          </a>
+          <a v-else class="content links" :href="lastWorkflowLink" target="_blank" :title="t('workflowCancelledDesc')">
+            <span>
+              {{ t("workflowCancelled") }}
+            </span>
+            <svg width="16" height="16" style="margin-top: 2px" class="octicon octicon-stop neutral-check" viewBox="0 0 16 16" version="1.1" role="img">
+              <path fill="#B7BDC8" d="M4.47.22A.749.749 0 0 1 5 0h6c.199 0 .389.079.53.22l4.25 4.25c.141.14.22.331.22.53v6a.749.749 0 0 1-.22.53l-4.25 4.25A.749.749 0 0 1 11 16H5a.749.749 0 0 1-.53-.22L.22 11.53A.749.749 0 0 1 0 11V5c0-.199.079-.389.22-.53Zm.84 1.28L1.5 5.31v5.38l3.81 3.81h5.38l3.81-3.81V5.31L10.69 1.5ZM8 4a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 8 4Zm0 8a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z">
+              </path>
+            </svg>
+          </a>
+        </div>
         <div class="content">
           <div>
             <input type="checkbox" class="checkbox" @change="changeTheme" id="chk" />
@@ -65,9 +105,9 @@ const { t } = useI18n();
               <div class="ball"></div>
             </label>
           </div>
-          <div class= "links">
+          <div class="links">
             <a href="/" target="_blank">
-              <font-awesome-icon icon="fa-solid fa-house" fixed-width alt="Home" />  
+              <font-awesome-icon icon="fa-solid fa-house" fixed-width alt="Home" />
             </a>
             <a @click="showDiscordEmbed">
               <font-awesome-icon icon="fa-brands fa-discord" fixed-width alt="Discord" />
@@ -90,7 +130,9 @@ const { t } = useI18n();
         <SelectGamePublisherPage v-else-if="currentPath == ''" />
         <NotFound v-else />
       </div>
-      <iframe v-if="discordEmbedShown" class="discordEmbed" src="https://discord.com/widget?id=1115634791321190420&theme=dark" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+      <iframe v-if="discordEmbedShown" class="discordEmbed"
+        src="https://discord.com/widget?id=1115634791321190420&theme=dark" allowtransparency="true" frameborder="0"
+        sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
     </div>
   </div>
   <footer>
@@ -104,7 +146,8 @@ export default {
     return {
       title: 'DataNRO',
       discordEmbedShown: false,
-      currentWorkflowLink: ""
+      lastWorkflowLink: "",
+      lastWorkflowStatus: ""  // in_progress, success, failure, cancelled
     }
   },
   methods: {
@@ -113,10 +156,10 @@ export default {
       if (location.pathname !== '/DataNRO/') {
         if (currentPath !== 'pageNotFound')
           document.title = 'DataNRO - Server ' + this.currentPath;
-        else 
+        else
           document.title = "DataNRO - " + useI18n().t(currentPath);
       }
-      else 
+      else
         document.title = "DataNRO by ElectroHeavenVN";
     },
     openNav() {
@@ -125,8 +168,7 @@ export default {
     closeNav() {
       document.getElementById("mySidenav").style.width = "0";
     },
-    setPage(page)
-    {
+    setPage(page) {
       window.history.pushState({}, '', window.location.origin + window.location.pathname + '?page=' + page);
       this.closeNav();
     },
@@ -144,7 +186,7 @@ export default {
       const workflowRuns = data.workflow_runs.filter(run => run.path === '.github/workflows/update-data.yml');
       const latestRun = workflowRuns[0];
       return latestRun;
-  }
+    }
   },
   computed: {
     currentPath() {
@@ -158,36 +200,39 @@ export default {
   },
   mounted() {
     this.changeTitle();
-    if (!localStorage.getItem("theme")) 
+    if (!localStorage.getItem("theme"))
       localStorage.setItem("theme", (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"));
     let theme = localStorage.getItem("theme");
     document.documentElement.setAttribute('data-theme', theme);
     document.getElementById('chk').checked = theme === 'light';
     this.getWorkflowStatus().then(data => {
-      if (data.status === "in_progress")
-        this.currentWorkflowLink = data.html_url;
+      this.lastWorkflowStatus = data.status;
+      this.lastWorkflowLink = data.html_url;
+      if (data.status !== "in_progress") {
+        this.lastWorkflowStatus = data.conclusion;
+      }
     });
   }
 }
 </script>
 
 <style scoped>
-
-.workflowRunning {
+.lastWorkflow {
   white-space: nowrap;
 }
 
 .anim-rotate {
-    animation: rotate-keyframes 1s linear infinite;
+  animation: rotate-keyframes 1s linear infinite;
 }
 
 @keyframes rotate-keyframes {
-    0% {
-        transform: rotate(0deg);
-    }
-    100% {
-        transform: rotate(360deg);
-    }
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
 }
 
 .discordEmbed {
@@ -206,55 +251,53 @@ export default {
 }
 
 .checkbox {
-	opacity: 0;
-	position: absolute;
+  opacity: 0;
+  position: absolute;
 }
 
 .label {
   border-style: solid;
   border-color: var(--component-border);
   border-width: 1px;
-	background-color: var(--component-color);
-	border-radius: 15px;
-	cursor: pointer;
-	display: flex;
-	align-items: center;
-	/* justify-content: space-between; */
-	padding: 3px;
-	position: relative;
-	height: 15px;
-	width: 35px;
+  background-color: var(--component-color);
+  border-radius: 15px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  /* justify-content: space-between; */
+  padding: 3px;
+  position: relative;
+  height: 15px;
+  width: 35px;
   gap: 5px;
 }
 
 .label .ball {
-	background-color: var(--component-bg);
-	border-radius: 50%;
-	position: absolute;
-	height: 18px;
-	width: 18px;
-	transform: translateX(0px);
-	transition: transform 0.2s linear;
+  background-color: var(--component-bg);
+  border-radius: 50%;
+  position: absolute;
+  height: 18px;
+  width: 18px;
+  transform: translateX(0px);
+  transition: transform 0.2s linear;
 }
 
-.checkbox:checked + .label .ball {
-	transform: translateX(17px);
+.checkbox:checked+.label .ball {
+  transform: translateX(17px);
 }
 
 .fa-moon {
-	color: #ffaa00;
+  color: #ffaa00;
 }
 
 .fa-sun {
-	color: #ff9900;
+  color: #ff9900;
 }
 
 .fa-discord {
   color: #5865F2;
-  filter: 
-    drop-shadow(1px 1px 1px #00ffff) 
-    drop-shadow(-1px 1px 1px #00ffff) 
-    drop-shadow(1px -1px 1px #00ffff) 
+  filter:
+    drop-shadow(1px 1px 1px #00ffff) drop-shadow(-1px 1px 1px #00ffff) drop-shadow(1px -1px 1px #00ffff)
     /* drop-shadow(-1px -1px 1px #48c2ff)  */
     !important;
 }
@@ -276,7 +319,7 @@ export default {
 
 .sidenav span,
 .sidenav a {
-  user-select: none;  
+  user-select: none;
   cursor: pointer;
   padding: 8px 8px 8px 32px;
   font-size: 20px;
@@ -295,15 +338,15 @@ export default {
 }
 
 .wrapper {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: calc(100% - 60px);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: calc(100% - 60px);
 }
 
 #main .content {
-    width: 100%;
-    margin-top: 20px;
+  width: 100%;
+  margin-top: 20px;
 }
 
 .links {
@@ -342,7 +385,7 @@ export default {
     width: calc(100% - 20px);
   }
 
-  #main .content {    
+  #main .content {
     width: calc(100% - 20px);
     margin-left: 10px;
   }
@@ -353,7 +396,7 @@ export default {
 }
 
 @media screen and (max-width: 450px) {
-  .workflowRunning span {
+  .lastWorkflow span {
     display: none;
   }
 }
